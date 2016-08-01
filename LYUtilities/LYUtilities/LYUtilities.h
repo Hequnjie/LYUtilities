6e5db59047f3058a4d5cc167f340455b83b635f1
Hequnjie <360606411@qq.com>
2016-08-01 11:22:36 +0800

    add LYUtilities project

---
+++ LYUtilities/LYUtilities/LYUtilities.h
@@ -0,0 +1,19 @@
+//
+//  LYUtilities.h
+//  LYUtilities
+//
+//  Created by Hequnjie on 16/8/1.
+//  Copyright © 2016年 Hequnjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LYUtilities.
+FOUNDATION_EXPORT double LYUtilitiesVersionNumber;
+
+//! Project version string for LYUtilities.
+FOUNDATION_EXPORT const unsigned char LYUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LYUtilities/PublicHeader.h>
+
+
