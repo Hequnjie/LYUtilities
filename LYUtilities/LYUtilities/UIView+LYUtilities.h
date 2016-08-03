b364498eb65265184886c750d731386bef2bffe2
Hequnjie <360606411@qq.com>
2016-08-03 16:45:31 +0800

    update UIView+LYUtilities

--- LYUtilities/LYUtilities/UIView+LYUtilities.h
+++ LYUtilities/LYUtilities/UIView+LYUtilities.h
@@ -10,9 +10,32 @@
 
 @interface UIView (LYUtilities)
 
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat right;
+@property (nonatomic, assign) CGFloat bottom;
+@property (nonatomic, assign) CGFloat top;
+
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+- (void)ly_verticalAlignTo:(nonnull UIView *)view;
+- (void)ly_horizontalAlignTo:(nonnull UIView *)view;
+
+
 #pragma mark - calculate size
 
 - (CGSize)ly_measure:(CGSize)constrainedSize;
 - (CGSize)ly_calculatedSize;
+
+
+#pragma mark - corner radius
+
+- (void)ly_setCornerRadius:(NSUInteger)radius;
+- (void)ly_setCornerRadius:(NSUInteger)radius withBackgroundColor:(nullable UIColor *)backgroundColor;
+- (void)ly_setCornerRadius:(NSUInteger)radius withBorderColor:(nullable UIColor *)borderColor borderWidth:(CGFloat)borderWidth;
+- (void)ly_setCornerRadius:(NSUInteger)radius withBackgroundColor:(nullable UIColor *)backgroundColor borderColor:(nullable UIColor *)borderColor borderWidth:(CGFloat)borderWidth;
+
 
 @end
--- LYUtilities/LYUtilities/UIView+LYUtilities.m
+++ LYUtilities/LYUtilities/UIView+LYUtilities.m
@@ -12,6 +12,104 @@
 
 @implementation UIView (LYUtilities)
 
+- (CGFloat)left{
+    return CGRectGetMinX(self.frame);
+}
+- (void)setLeft:(CGFloat)left{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.origin.x = left;
+        frame;
+    });
+}
+
+- (CGFloat)right{
+    return CGRectGetMaxX(self.frame);
+}
+- (void)setRight:(CGFloat)right{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.origin.x += right - self.right;
+        frame;
+    });
+}
+
+- (CGFloat)bottom{
+    return CGRectGetMaxY(self.frame);
+}
+- (void)setBottom:(CGFloat)bottom{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.origin.y += bottom - self.bottom;
+        frame;
+    });
+}
+
+- (CGFloat)top{
+    return CGRectGetMinY(self.frame);
+}
+- (void)setTop:(CGFloat)top{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.origin.y = top;
+        frame;
+    });
+}
+
+- (CGFloat)width{
+    return CGRectGetWidth(self.frame);
+}
+- (void)setWidth:(CGFloat)width{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.size.width = width;
+        frame;
+    });
+}
+
+- (CGFloat)height{
+    return CGRectGetHeight(self.frame);
+}
+
+- (void)setHeight:(CGFloat)height{
+    self.frame = ({
+        CGRect frame = self.frame;
+        frame.size.height = height;
+        frame;
+    });
+}
+
+- (CGFloat)centerX {
+    return self.center.x;
+}
+
+- (void)setCenterX:(CGFloat)centerX {
+    self.center = CGPointMake(centerX, self.center.y);
+}
+
+- (CGFloat)centerY {
+    return self.center.y;
+}
+
+- (void)setCenterY:(CGFloat)centerY {
+    self.center = CGPointMake(self.center.x, centerY);
+}
+
+- (void)ly_verticalAlignTo:(nonnull UIView *)view {
+    self.center = ({
+        CGPoint center = self.center;
+        center.x =  view.center.x;
+        center;
+    });
+}
+
+- (void)ly_horizontalAlignTo:(nonnull UIView *)view {
+    self.center = ({
+        CGPoint center = self.center;
+        center.y =  view.center.y;
+        center;
+    });
+}
 
 #pragma mark - calculate size
 
@@ -37,6 +135,30 @@ static void *kCalculatedSizeIdentifier = &kCalculatedSizeIdentifier;
     CGSize sizeThatFits = [self sizeThatFits:constrainedSize];
     self.ly_calculatedSize = sizeThatFits;
     return sizeThatFits;
+}
+
+
+#pragma mark - corner radius
+
+- (void)ly_setCornerRadius:(NSUInteger)radius {
+    [self ly_setCornerRadius:radius withBackgroundColor:self.backgroundColor];
+}
+
+- (void)ly_setCornerRadius:(NSUInteger)radius withBackgroundColor:(nullable UIColor *)backgroundColor {
+    [self ly_setCornerRadius:radius withBackgroundColor:backgroundColor borderColor:nil borderWidth:0];
+}
+
+- (void)ly_setCornerRadius:(NSUInteger)radius withBorderColor:(nullable UIColor *)borderColor borderWidth:(CGFloat)borderWidth {
+    [self ly_setCornerRadius:radius withBackgroundColor:nil borderColor:borderColor borderWidth:borderWidth];
+}
+
+- (void)ly_setCornerRadius:(NSUInteger)radius withBackgroundColor:(nullable UIColor *)backgroundColor borderColor:(nullable UIColor *)borderColor borderWidth:(CGFloat)borderWidth {
+    self.layer.cornerRadius = radius;
+    self.layer.backgroundColor = [backgroundColor CGColor];
+    self.layer.borderColor = [borderColor CGColor];
+    self.layer.borderWidth = borderWidth;
+    
+    self.layer.masksToBounds = YES;
 }
 
 
